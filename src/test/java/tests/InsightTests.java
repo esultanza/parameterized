0c97a928e153f0de446dcac4281288d547b84f0e
esultanza <taya4@ya.ru>
2020-08-22 23:30:30 +0300

    Update InsightTests.java

--- src/test/java/tests/InsightTests.java
+++ src/test/java/tests/InsightTests.java
@@ -13,7 +13,7 @@ public class InsightTests {
 
 
     @Test
-    @Tag("insight_test")
+    @Tag("insight_tag")
     void paramitrizedStringTest() {
         String item = System.getProperty("item", "reminisce"); // from terminal, gradle insight_test -Dbbb=reminisce
         logger.info("\nitem = " + item);
